365471ed105263811c5e9697b21824ce66021990
JL <johnlloydguevarra0405@gmail.com>
2025-02-18 13:25:48 +0800

    jl

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(CupertinoApp(
     debugShowCheckedModeBanner: false,
-    home: MyApp(),));
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -15,110 +16,107 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
-
-
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text('Activity in DevOps'),
-          trailing: CupertinoButton(child: Icon(CupertinoIcons.info, size: 25,)
-              , onPressed: (){
-
-
-
-
-
-
-                showCupertinoDialog(
-                  context: context,
-                  builder: (context) {
-                    return CupertinoAlertDialog(
-                      title: Text('Members'),
-                      content: Container(
-                        height: 200,
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  ClipOval(child: Image.asset('images/ChristianCaparra.jpg', height: 50)),
-                                  SizedBox(width: 10),
-                                  Text('Christian Caparra'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  ClipOval(child: Image.asset('images/JL.jpg', height: 50)),
-                                  SizedBox(width: 10),
-                                  Text('John Lloyd Guevarra'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  ClipOval(child: Image.asset('images/ChristianCaparra.jpg', height: 50)),
-                                  SizedBox(width: 10),
-                                  Text('Christian Caparra'),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Row(
-                                children: [
-                                  ClipOval(child: Image.asset('images/mike.jpg', height: 50)),
-                                  SizedBox(width: 10),
-                                  Text('Michael De Ramos'),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Activity in DevOps'),
+        trailing: CupertinoButton(
+          child: Icon(CupertinoIcons.info, size: 25),
+          onPressed: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  title: Text('Members'),
+                  content: Container(
+                    height: 200,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          _buildMemberRow('images/ChristianCaparra.jpg', 'Christian Caparra'),
+                          SizedBox(height: 10),
+                          _buildMemberRow('images/JL.jpg', 'John Lloyd Guevarra'),
+                          SizedBox(height: 10),
+                          _buildMemberRow('images/ChristianCaparra.jpg', 'Christian Caparra'),
+                          SizedBox(height: 10),
+                          _buildMemberRow('images/mike.jpg', 'Michael De Ramos'),
+                        ],
                       ),
-                      actions: [
-                        CupertinoButton(
-                          child: Text(
-                            'Close',
-                            style: TextStyle(color: CupertinoColors.destructiveRed),
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-
-
-              }),
-
-        ),
-
-
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    ClipOval(child: Image.asset('images/JL.jpg', height: 100,)),
+                    ),
+                  ),
+                  actions: [
+                    CupertinoButton(
+                      child: Text(
+                        'Close',
+                        style: TextStyle(color: CupertinoColors.destructiveRed),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
                   ],
-                )
-                
-
-
-
-
-              ],
-            ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipOval(
+                    child: Image.asset(
+                      'images/JL.jpg',
+                      height: 50,
+                      width: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name: John Lloyd Guevarra',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text('Email: johnlloydguevarra0405@gmail.com'),
+                        Text('Address: San Nicolas, San Luis, Pampanga'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  // Helper method to create a member row for the dialog
+  Widget _buildMemberRow(String imagePath, String name) {
+    return Row(
+      children: [
+        ClipOval(
+          child: Image.asset(
+            imagePath,
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(width: 10),
+        Text(name),
+      ],
+    );
   }
 }
