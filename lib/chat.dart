566f1e04b9baf4891b27c0d367be14444135f029
JL <johnlloydguevarra0405@gmail.com>
2025-02-21 18:06:43 +0800

    jl

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'jl_chat_page.dart';
 
 import 'christian_chat_page.dart';
 import 'jhuniel_chat_page.dart';
@@ -265,7 +266,7 @@ class _ChatState extends State<Chat> {
 
 
                   GestureDetector(
-                    onTap: () => _showStoryDialog(context, "Samuel Miranda", "images/JL.jpg"),
+                    onTap: () => _showStoryDialog(context, "Samuel Miranda", "images/mike.jpg"),
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8),
                       child: Column(
@@ -277,7 +278,7 @@ class _ChatState extends State<Chat> {
                             ),
                             child: ClipOval(
                               child: Image.asset(
-                                "images/JL.jpg",
+                                "images/mike.jpg",
                                 height: 75,
                                 width: 75,
                                 fit: BoxFit.cover,
@@ -328,10 +329,19 @@ class _ChatState extends State<Chat> {
                               builder: (context) => const JhunielChatPage()),
                         );
 
-                        //dito nyo add sainyo gamit kayo else if
 
-                        //
                       }
+                      else if (participant["name"] == "John Lloyd Guevarra") {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const JlChatPage()),
+                        );
+
+
+
+
+                      } //dito nyo add sainyo gamit kayo else if
 
 
 
@@ -381,6 +391,7 @@ class _ChatState extends State<Chat> {
                                       ),
                                   ],
                                 ),
+
 
 
 
@@ -455,9 +466,9 @@ class _ChatState extends State<Chat> {
 List<Map<String, String>> participants = [
   {"name": "Christian Caparra", "image": "images/ChristianCaparra.jpg"},
   {"name": "Jhuniel Galang", "image": "images/Jhuniel.jpg"},
-  {"name": "John Lloyd Guevarra", "image": "images/JL.jpg"},
+  {"name": "John Lloyd Guevarra", "image": "images/JL1.jpg"},
   {"name": "Michael De Ramos", "image": "images/mike.jpg"},
-  {"name": "Samuel Miranda", "image": "images/JL.jpg"},
+  {"name": "Samuel Miranda", "image": "images/mike.jpg"},
 
 
 
